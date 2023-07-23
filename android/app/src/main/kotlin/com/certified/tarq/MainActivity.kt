708b016c8fd4f333aa60490988bce322b8e2b0ac
Samson Achiaga <sirhumble84@gmail.com>
2023-07-23 16:01:30 +0100

    @update/Update Manifest and build.gradle file

--- android/app/src/main/kotlin/com/certified/tarq/MainActivity.kt
+++ android/app/src/main/kotlin/com/certified/tarq/MainActivity.kt
@@ -1,6 +1,12 @@
-package com.example.tarq
+package com.certified.tarq
 
 import io.flutter.embedding.android.FlutterActivity
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen()
+    }
 }
